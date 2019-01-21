9451713ab146b1bc39f143fbb5e4c39a654b51b1
DanWipf <wipf.daniel@gmx.ch>
2019-01-21 09:55:31 +0100

    v0.11
    Simple bug removed, Re-Importing only for SceneObjects(further Investigation why it' cant be as well assets.)

--- PivotAdjustment/SFB_StructSetup.cs
+++ PivotAdjustment/SFB_StructSetup.cs
@@ -7,19 +7,23 @@ using System.Linq;
 namespace SimpleFenceBuilder{
 	public static class SFB_StructSetup {
 		
-		public static void ReImport(this Object go){
-			string realPath = Application.dataPath;
-			realPath.Remove(realPath.Length -6);
-			string selectedPath = realPath + AssetDatabase.GetAssetPath(go);
-			string[] fileEntries = Directory.GetFiles(selectedPath,"*",SearchOption.AllDirectories);
-			for(int i = 0; i<fileEntries.Length; i++){
-				string file = fileEntries[i];
-				file = file.Replace("\\","/");
-				file = file.Remove(0,realPath.Length);
-				if(file.Contains(PrefabUtility.GetCorrespondingObjectFromSource(go).name) && !file.Contains(".meta")){
-					AssetDatabase.ImportAsset("Assets"+file);
-					Debug.Log(" Object was succesfully reimported at: "+"<color=#e0771a><i>"+"Assets"+file+"</i></color>");
+		public static void ReImport(this GameObject go){
+			if(go.scene.IsValid()){
+				string realPath = Application.dataPath;
+				realPath.Remove(realPath.Length -6);
+				string selectedPath = realPath + AssetDatabase.GetAssetPath(go);
+				string[] fileEntries = Directory.GetFiles(selectedPath,"*",SearchOption.AllDirectories);
+				for(int i = 0; i<fileEntries.Length; i++){
+					string file = fileEntries[i];
+					file = file.Replace("\\","/");
+					file = file.Remove(0,realPath.Length);
+					if(file.Contains(PrefabUtility.GetCorrespondingObjectFromSource(go).name) && !file.Contains(".meta")){
+						AssetDatabase.ImportAsset("Assets"+file);
+						Debug.Log(" Object was succesfully reimported at: "+"<color=#e0771a><i>"+"Assets"+file+"</i></color>");
+					}
 				}
+			}else{
+					Debug.Log("Can Only Re-Import Scene Objects.");
 			}
 		}
 		public static PivotTransform[] SFB_ArraySetup(this PivotTransform[] pt,bool always = false){
@@ -33,7 +37,7 @@ namespace SimpleFenceBuilder{
 			
 			if(pt.Pivot_Transform != null){
 				if(pt.ReImport){
-					ReImport(pt.Pivot_Transform);
+					ReImport(pt.Pivot_Transform.gameObject);
 					pt.ReImport = false;
 				}
 				if(pt.ChangePivot){
